95f2d8e4d1a31710fa3ce92218eaecf4c253aa60
blkcor <blkcor.dev@gmail.com>
2024-01-23 10:15:48 +0800

    feat: 新增logback日志配置

--- train-member/src/main/java/com/github/blkcor/TrainMemberApplication.java
+++ train-member/src/main/java/com/github/blkcor/TrainMemberApplication.java
@@ -1,12 +1,21 @@
 package com.github.blkcor;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication(scanBasePackages = "com.github.blkcor")
 public class TrainMemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(TrainMemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(TrainMemberApplication.class, args);
+        SpringApplication app = new SpringApplication(TrainMemberApplication.class);
+        ConfigurableEnvironment environment = app.run(args).getEnvironment();
+        LOG.info("启动成功!");
+        LOG.info("地址: \thttp://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 }
