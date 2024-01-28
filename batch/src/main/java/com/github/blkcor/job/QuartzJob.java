bee5af374519f470910d6bdbe1ea54bcfbe31b25
blkcor <blkcor.dev@gmail.com>
2024-01-28 16:41:20 +0800

    chore: 编写供应quartz调度的job

--- batch/src/main/java/com/github/blkcor/job/QuartzJob.java
+++ batch/src/main/java/com/github/blkcor/job/QuartzJob.java
@@ -1,15 +1,15 @@
-//package com.github.blkcor.job;
-//
-//import org.quartz.DisallowConcurrentExecution;
-//import org.quartz.Job;
-//import org.quartz.JobExecutionContext;
-//import org.quartz.JobExecutionException;
-//
-//@DisallowConcurrentExecution
-//public class QuartzJob implements Job {
-//
-//    @Override
-//    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-//        System.out.println("QuartzJob execute");
-//    }
-//}
+package com.github.blkcor.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+@DisallowConcurrentExecution
+public class QuartzJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("QuartzJob execute");
+    }
+}
