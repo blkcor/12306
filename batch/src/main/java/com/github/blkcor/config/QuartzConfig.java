1d52201e9951a4a82ad45848f5828adc2c3d7ded
blkcor <blkcor.dev@gmail.com>
2024-01-28 16:02:17 +0800

    chore: 添加quartz sql，并注释声明式任务调度的方法

--- batch/src/main/java/com/github/blkcor/config/QuartzConfig.java
+++ batch/src/main/java/com/github/blkcor/config/QuartzConfig.java
@@ -1,35 +1,35 @@
-package com.github.blkcor.config;
-
-import com.github.blkcor.job.QuartzJob;
-import org.quartz.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class QuartzConfig {
-    /**
-     * 声明一个任务
-     */
-    @Bean
-    public JobDetail jobDetail(){
-        return JobBuilder.newJob(QuartzJob.class)
-                .withIdentity("quartzJob", "quartzJobGroup")
-                .storeDurably()
-                .build();
-    }
-
-    /**
-     * 声明一个触发器，即什么时候触发这个任务
-     */
-    @Bean
-    public Trigger trigger(){
-        return TriggerBuilder.newTrigger()
-                .forJob(jobDetail())
-                .withIdentity("quartzJobTrigger", "quartzJobTriggerGroup")
-                .startNow()
-                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
-                        .withIntervalInSeconds(5)
-                        .repeatForever())
-                .build();
-    }
-}
+//package com.github.blkcor.config;
+//
+//import com.github.blkcor.job.QuartzJob;
+//import org.quartz.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class QuartzConfig {
+//    /**
+//     * 声明一个任务
+//     */
+//    @Bean
+//    public JobDetail jobDetail(){
+//        return JobBuilder.newJob(QuartzJob.class)
+//                .withIdentity("quartzJob", "quartzJobGroup")
+//                .storeDurably()
+//                .build();
+//    }
+//
+//    /**
+//     * 声明一个触发器，即什么时候触发这个任务
+//     */
+//    @Bean
+//    public Trigger trigger(){
+//        return TriggerBuilder.newTrigger()
+//                .forJob(jobDetail())
+//                .withIdentity("quartzJobTrigger", "quartzJobTriggerGroup")
+//                .startNow()
+//                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+//                        .withIntervalInSeconds(5)
+//                        .repeatForever())
+//                .build();
+//    }
+//}
