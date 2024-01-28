fe118fb30c96e98b110591554fd0d3bf9cd35d50
blkcor <blkcor.dev@gmail.com>
2024-01-28 15:50:38 +0800

    feat: 集成quartz并编写demo

---
+++ batch/src/main/java/com/github/blkcor/job/QuartzJob.java
@@ -0,0 +1,13 @@
+package com.github.blkcor.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class QuartzJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("QuartzJob execute");
+    }
+}
