c3f77c52937b387ed0bef0b104a5efe21ae5b4db
blkcor <blkcor.dev@gmail.com>
2024-01-28 15:53:26 +0800

    chore: 禁止定时任务并发执行

--- batch/src/main/java/com/github/blkcor/job/QuartzJob.java
+++ batch/src/main/java/com/github/blkcor/job/QuartzJob.java
@@ -1,9 +1,11 @@
 package com.github.blkcor.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class QuartzJob implements Job {
 
     @Override
