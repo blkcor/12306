0253e90210fb3a682ebb7e7e5dbf4d89e5af1964
blkcor <blkcor.dev@gmail.com>
2024-02-12 21:30:10 +0800

    feat: springboot整合redis

--- business/src/main/java/com/github/blkcor/controller/HelloController.java
+++ business/src/main/java/com/github/blkcor/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.github.blkcor.controller;
 
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class HelloController {
 
+    @Resource
+    private RedisTemplate<String,String> redisTemplate;
     @GetMapping("/hello")
     public String hello() {
+        redisTemplate.opsForValue().set("hello","blkcor");
         return "Hello World!";
+    }
+
+    @GetMapping("/hello/get")
+    public String hello1() {
+        return redisTemplate.opsForValue().get("hello");
     }
 }
