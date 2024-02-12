59ab6c0b78bb605a3491c92ea44fc5720fbcbca9
blkcor <blkcor.dev@gmail.com>
2024-02-12 18:15:09 +0800

    feat: nacos+gateway实现服务的load-balance

--- member/src/main/java/com/github/blkcor/controller/TestController.java
+++ member/src/main/java/com/github/blkcor/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.github.blkcor.controller;
 
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @Value("${test.name}")
     private String name;
+
+    @Resource
+    private Environment environment;
     @GetMapping("/hello")
     public String hello(){
-        return name;
+        String port = environment.getProperty("local.server.port");
+        return name + "端口:" + port;
     }
 }
